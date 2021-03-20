549d12017b96df9a28712e64df732734369df70d
Aníbal <anbal.mc@hotmail.com>
2021-03-20 03:57:29 +0100

    Add vendor files for lineage 15.1

--- Android.mk
+++ Android.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2018-2020 The LineageOS Project
+# Copyright (C) 2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,6 +16,16 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter crownlte star2lte starlte,$(TARGET_DEVICE)),)
+ifneq ($(filter star2lte starlte,$(TARGET_DEVICE)),)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := cbd
+LOCAL_MODULE_OWNER := samsung
+LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT_SBIN)
+LOCAL_UNSTRIPPED_PATH := $(TARGET_ROOT_OUT_SBIN_UNSTRIPPED)
+LOCAL_SRC_FILES := proprietary/rootfs/sbin/cbd
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := EXECUTABLES
+include $(BUILD_PREBUILT)
 
 endif
